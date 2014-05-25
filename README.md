2789f356006d8062f41b79ce06c12107a7d426a2
bogerm <buklanov@gmail.com>
2014-05-25 22:41:45 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+In order to use this file you should download "Human Activity Recognition Using Smartphones" dataset http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+exctract archive into current folder and run script run_analysis.R
+The output will be generated in file called tidy.txt
---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+activity_labels <- read.table("activity_labels.txt", header=FALSE, sep=" ",stringsAsFactors=FALSE)
+features_labels <- read.table("features.txt",header=FALSE, sep=" ",stringsAsFactors=FALSE)
+names(features_labels) <- c("id", "features")
+x_train <- read.table("./train/X_train.txt")
+x_test <- read.table("./test/X_test.txt")
+y_train <- read.table("./train/y_train.txt")
+y_test <- read.table("./test/y_test.txt")
+subject_train <- read.table("./train/subject_train.txt")
+subject_test <- read.table("./test/subject_test.txt")
+
+data_train <- cbind.data.frame(subject_train, y_train, x_train)
+data_test <- cbind.data.frame(subject_test, y_test, x_test)
+
+data <- rbind(data_test, data_train)
+names(data) <- c("subject_id", "activity", features_labels$features)
+names(data)
+
+mean_std_index <- sort(union(grep("mean", names(data), fixed=T), grep("std", names(data), fixed=T)))
+data_mean_std <- data[, mean_std_index]
+
+write.table(data_mean_std,"tidy.txt",sep=" ")
